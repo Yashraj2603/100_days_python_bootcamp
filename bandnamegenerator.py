1f98b24e73cbff793cdbf5252600f2e42a9b813f
Yash Raj <55595124+Yashraj2603@users.noreply.github.com>
2023-10-20 15:29:26 +0530

    Create bandnamegenerator.py
    Simple python program

---
+++ bandnamegenerator.py
@@ -0,0 +1,9 @@
+def band_name_generator(city , pet):
+    print("Your band name can be ", city + pet)
+
+
+def query_band():
+    city = input("Enter name of your city ")
+    pet = input("Enter name of your pet ")
+    band_name_generator(city,pet)
+query_band()
